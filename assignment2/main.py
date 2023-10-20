073c5776f543b4e9b0f3dcbacc35324ec9e53e06
Yujia (Hazel) Li <48962522+Hazelilili@users.noreply.github.com>
2023-10-20 11:04:46 -0700

    Update main.py

--- assignment2/main.py
+++ assignment2/main.py
@@ -17,7 +17,20 @@ def draw():
   p5.noStroke()
   p5.fill(150)
   p5.ellipse(150, 150, circle_size, circle_size)
-
+  p5.push()
+  # set angle variable to integer of data
+  angle = int(data)
+  # move to middle of canvas
+  p5.translate(p5.width/2, p5.height/2)
+  # rotate canvas with angle converted from degrees to radiant:
+  p5.rotate(p5.radians(angle))
+  #change mode to draw rectangle from center:
+  p5.rectangle(p5.center)
+  # draw rectangle at coordinate 0, 0 and 100 width and height:
+  p5.rectangle(0,0,100,100)
+  # restore graphical transformation
+  p5.pop()
+  
 def print_test(x):
   print(x)
 
