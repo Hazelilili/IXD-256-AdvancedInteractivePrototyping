996c1a76a761146120042e2beaa3ba528abe3956
Yujia (Hazel) Li <48962522+Hazelilili@users.noreply.github.com>
2023-10-27 09:06:30 -0700

    update

--- assignment2/software_template/main.py
+++ assignment2/software_template/main.py
@@ -5,56 +5,64 @@ data_string = None
 data_list = None
 light_val = None
 angle_val = None
+falling_circles = []
 
-# load image data and assign it to variable:
+# Load the image data and assign it to a variable
 plant_img = p5.loadImage('plant.png')
 
 def setup():
-  p5.createCanvas(300, 300)
-  # change mode to draw rectangles from center:
-  p5.rectMode(p5.CENTER)
-  # change mode to draw images from center:
-  p5.imageMode(p5.CENTER)
-  # change stroke cap to square:
-  p5.strokeCap(p5.SQUARE)
+    p5.createCanvas(300, 300)
+    p5.imageMode(p5.CENTER)
+
+def draw_background_gradient():
+    global light_val
+    p5.noStroke()
+
+    # Image drawing
+    img_width = p5.width
+    img_height = p5.width * (plant_img.height / plant_img.width)  # maintain original aspect ratio
+    p5.image(plant_img, p5.width / 2, p5.height / 2, img_width, img_height)
+
+    # Create linear gradient based on light sensor
+    if light_val > 127:
+        for y in range(300):
+            n = p5.map(y, 0, 299, 0, light_val - 127)
+            p5.stroke(0, 0, 0, 255 - n * 2)
+            p5.line(0, y, 300, y)
+    else:
+        for y in range(300):
+            n = p5.map(y, 0, 299, light_val, 0)
+            p5.stroke(0, 0, 0, 255 - n * 2)
+            p5.line(0, y, 300, y)
 
 def draw():
-  p5.background(0)
-  global data_string, data_list
-  global light_val, angle_val
+    global data_string, data_list
+    global light_val, angle_val
+    global falling_circles
 
-  # assign content of "data" div on index.html page to variable:
-  data_string = document.getElementById("data").innerText
-  # split data_string by comma, making a list:
-  data_list = data_string.split(',')
+    # Draw background gradient based on light sensor value
+    draw_background_gradient()
 
-  # assign 1st item of data_list to sensor_val:
-  light_val = int(data_list[0])
-  # assign 2nd item of data_list to sensor_val:
-  angle_val = int(data_list[1])
-  
-  # draw circle changing size with sensor data:
-  # ellipse function takes (x, y, width, height)
-  # map function takes (value, in_min, in_max, out_min, out_max)
-  circle_size = p5.map(light_val, 0, 255, 25, 100)
-  p5.ellipse(75, 75, circle_size, circle_size)
-  
-  # draw square changing color with sensor data:
-  # fill function can take (red, green, blue)
-  p5.fill(light_val, 0, 255 - light_val)  
-  # rectangle function takes (x, y, width, height)
-  p5.rect(225, 75, 100, 100)
+    # Parse sensor data
+    data_string = document.getElementById("data").innerText
+    data_list = data_string.split(',')
+    light_val = int(data_list[0])
+    angle_val = int(data_list[1])
 
-  
-  # draw lines responding to button data:
-  for i in range(20):
-    if(angle_val == 0): 
-      p5.strokeWeight(i+1)
-    else: 
-      p5.strokeWeight(8-i)
-    p5.stroke(0)
-    # line function takes (x1, y1, x2, y2)
-    x1 = x2 = 25 + i * 14
-    y1 = 175
-    y2 = 275
-    p5.line(x1, y1, x2, y2)
+    # Falling circles based on angle sensor
+    num_new_circles = p5.map(angle_val, 0, 255, 0, 5)
+    for _ in range(int(num_new_circles)):
+        falling_circles.append({
+            "x": p5.random(0, 300),
+            "y": -10,  # starts a bit outside of canvas
+            "r": p5.random(5, 25),
+            "speed": p5.random(1, 3)
+        })
+
+    p5.fill(0, 0, 255)
+    p5.noStroke()
+    for circle in falling_circles:
+        p5.ellipse(circle["x"], circle["y"], circle["r"], circle["r"])
+        circle["y"] += circle["speed"]
+    
+   
