60506286f851b895104b1c3fe28ae26b48e58c45
Yujia (Hazel) Li <48962522+Hazelilili@users.noreply.github.com>
2023-10-27 08:52:34 -0700

    commit poroject 2

--- assignment2/software_template/main.py
+++ assignment2/software_template/main.py
@@ -1,54 +1,60 @@
 import js as p5
 from js import document
 
-# 这些是您的全局变量
 data_string = None
 data_list = None
-
 light_val = None
 angle_val = None
-png_image = None  # 用于加载和显示png图像
 
-def preload():
-    global png_image
-    png_image = p5.loadImage('https://images.unsplash.com/photo-1501004318641-b39e6451bec6?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGxhbnR8ZW58MHx8MHx8fDA%3D')  # 请替换为您的png图像的路径
+# load image data and assign it to variable:
+plant_img = p5.loadImage('plant.png')
 
 def setup():
-    p5.createCanvas(300, 300)
-    p5.angleMode(p5.DEGREES)
+  p5.createCanvas(300, 300)
+  # change mode to draw rectangles from center:
+  p5.rectMode(p5.CENTER)
+  # change mode to draw images from center:
+  p5.imageMode(p5.CENTER)
+  # change stroke cap to square:
+  p5.strokeCap(p5.SQUARE)
 
 def draw():
-    global data_string, data_list
-    global light_val, angle_val
-    
-    # 从HTML元素获取数据
-    data_string = document.getElementById("data").innerText
-    data_list = data_string.split(',')
-    
-    if len(data_list) >= 2:
-        light_val = int(data_list[0])
-        angle_val = int(data_list[1])
-    
-    # 背景渐变
-    for y in range(300):
-        grad_color = p5.lerpColor(p5.color(0, 0, 0), p5.color(255, 255, 255), y/300 * (light_val/255))
-        p5.stroke(grad_color)
-        p5.line(0, y, 300, y)
-    
-    # 绘制png图像
-    p5.image(png_image, 0, 0, p5.width, p5.height)
-    
-    # 根据angle_val绘制下落的圆
-    num_circles = p5.map(angle_val, 0, 255, 0, 50)  # 您可以根据需要调整这里的范围
-    p5.fill(p5.color(0, 0, 255))
-    p5.noStroke()
-    
-    for _ in range(int(num_circles)):
-        x = p5.random(0, 300)
-        y = p5.random(250, 300)
-        r = p5.random(5, 25)
-        p5.ellipse(x, y, r, r)
+  p5.background(0)
+  global data_string, data_list
+  global light_val, angle_val
 
-# 剩下的代码应当是与您的硬件设备进行交互以获取sensor数据的部分
+  # assign content of "data" div on index.html page to variable:
+  data_string = document.getElementById("data").innerText
+  # split data_string by comma, making a list:
+  data_list = data_string.split(',')
 
+  # assign 1st item of data_list to sensor_val:
+  light_val = int(data_list[0])
+  # assign 2nd item of data_list to sensor_val:
+  angle_val = int(data_list[1])
+  
+  # draw circle changing size with sensor data:
+  # ellipse function takes (x, y, width, height)
+  # map function takes (value, in_min, in_max, out_min, out_max)
+  circle_size = p5.map(light_val, 0, 255, 25, 100)
+  p5.ellipse(75, 75, circle_size, circle_size)
+  
+  # draw square changing color with sensor data:
+  # fill function can take (red, green, blue)
+  p5.fill(light_val, 0, 255 - light_val)  
+  # rectangle function takes (x, y, width, height)
+  p5.rect(225, 75, 100, 100)
 
+  
+  # draw lines responding to button data:
+  for i in range(20):
+    if(angle_val == 0): 
+      p5.strokeWeight(i+1)
+    else: 
+      p5.strokeWeight(8-i)
+    p5.stroke(0)
+    # line function takes (x1, y1, x2, y2)
+    x1 = x2 = 25 + i * 14
+    y1 = 175
+    y2 = 275
+    p5.line(x1, y1, x2, y2)
